f3ea63198b95ebe710453ae3747e8187dd285cdd
kamruzzaman leeon <leeon.ewubd@gmail.com>
2020-05-05 14:32:39 +0600

    Revert "rms"
    This reverts commit 0b6db608b552b843cd9fd4c0eb66d9f66dc4ec6e.

---
+++ README.md
@@ -0,0 +1,7 @@
+# rms
+rms means restaurant management system
+
+To run this project,we need xamapp.
+download this rms folder then unzip into hhtdoc floder,which is place on xamapp install floder.
+then run xamapp apache & mysql.
+open any browser then type http://localhost/(location) (for example http://localhost/rms/)
--- backreservation.php
+++ backreservation.php
@@ -34,6 +34,50 @@ if(isset($_POST['date']) && isset($_POST['person']) && isset($_POST['slot']) && 
 	$query="INSERT INTO `reservation`(`date`, `slot`, `person`, `status`) VALUES ($date','$slot','$person','$status')";
 	mysqli_query($con,$query);
 }
+//delete food item 
+// if(isset($_POST['deleteid'])){
+// 	$foodid=$_POST['deleteid'];
+// 	$deletequery="DELETE FROM `food` WHERE food_id='$foodid' ";
+// 	mysqli_query($con,$deletequery);
+// }
+// //edit food
+// if(isset($_POST['editid']) && isset($_POST['editid'])!= ""){
+// 	$foodid=$_POST['editid'];
+// 	$query= "SELECT * FROM `food` WHERE food_id = '$foodid'";
+// 	if(!$result=mysqli_query($con,$query)){
+// 		exit(mysqli_error());
+// 	}
+
+// 	$response=array();
+
+// 	if(mysqli_num_rows($result)>0){
+// 		while ($row=mysqli_fetch_assoc($result)) {
+// 			$response=$row;
+// 		}
+// 	}else{
+// 		$response['status']=200;
+// 		$response['message']="Data not found!";
+// 	}
+// 	echo json_encode($response);
+// }
+// else{
+// 	$response['status']=200;
+// 	$response['message']="Invalid Request!";
+// }
+
+// //update table
+// if(isset($_POST['updatehidden_food_id'])){
+
+// 	$updatehidden_food_id = $_POST['updatehidden_food_id'];
+// 	$updatefoodname = $_POST['updatefoodname'];
+// 	$updatefoodtype = $_POST['updatefoodtype'];
+// 	$updatedescription = $_POST['updatedescription'];
+// 	$updateprice = $_POST['updateprice'];
+// 	$updateavailability=$_POST['updateavailability'];
+
+// 	$query=" UPDATE `food` SET `foodname`='$updatefoodname',`foodtype`='$updatefoodtype',`description`='$updatedescription',`price`='$updateprice' ,`availability`='$updateavailability' WHERE food_id='$updatehidden_food_id'";
+// 	mysqli_query($con,$query);
+// }
 
 
 ?>
